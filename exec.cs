728b6c2b7be6b8f6f658a4085db38a9eb24488c2
Anna Clara Medina Roissmann <anna.medina28@unifesp.br>
2026-04-10 23:48:18 -0300

    adição de proxy e de adaptador

--- exec.cs
+++ exec.cs
@@ -1,81 +1,155 @@
-class globais{
+class globais
+{
 
     private static globais instance;
-    private String nomeAplicacao;
-    private String servidor;
-    private int maxTentativas;
+    private string NomeAplicacao { get; set { string nome = "Minha Aplicação"; } }
+    private string Servidor { get; set { string servidor = "smtp.exemplo.com"; } }
+    private int MaxTentativas { get; set { string max = 3; } }
 
-    private globais(){
-        this.nomeAplicacao = "Minha Aplicação";
-        this.servidor = "smtp.exemplo.com";
-        this.maxTentativas = 3;
-    }
+    private globais() { }
 
-    public static globais getInstances(){
-        if(instance == null){
+    public static globais getInstances()
+    {
+        if (instance == null)
+        {
             instance = new globais();
         }
         return instance;
     }
-
-    public String getNomeAplicacao(){
-        return this.nomeAplicacao;
-    }
-
-    public String getServidor(){
-        return this.servidor;
-    }
-
-    public int getMaxTentativas(){
-        return this.maxTentativas;
-    }
-
 }
 
-class Notifications{
-     void enviar(String destinatario){}
+class Notifications
+{
+    public void Enviar(string destinatario) { }
 }
 
-class enviarEmail implements Notifications{
+class EnviarEmail : Notifications
+{
 
-    public void enviarEmail(String destinatario){
+    public void EnviarEmail(string destinatario)
+    {
         globais g = globais.getInstances();
-        g.getNomeAplicacao();
-        g.getServidor();
-        g.getMaxTentativas();
     }
 }
 
-class enviarSMS implements Notifications{
+class EmailExterno
+{
+    public void SendEmail(string destinatario) { }
+}
 
-    public void enviarSMS(String destinatario){
+class AdaptadorEmail : Notifications
+{
+    private EmailExterno email;
+
+    public AdaptadorEmail(EmailExterno email)
+    {
+        this.email = email;
+    }
+
+    public override void Enviar(string destinatario)
+    {
+        email.SendEmail(destinatario);
+    }
+}
+
+class EnviarSMS : Notifications
+{
+
+    public void EnviarSMS(string destinatario)
+    {
         globais g = globais.getInstances();
-        g.getNomeAplicacao();
-        g.getServidor();
-        g.getMaxTentativas();
     }
 }
 
-class enviarPush implements Notifications{
+class SMSExterno
+{
+    public void SendSMS(string destinatario) { }
+}
 
-    public void enviarPush(String destinatario){
+class AdaptadorSMS : Notifications
+{
+    private SMSExterno sms;
+
+    public AdaptadorSMS(SMSExterno sms)
+    {
+        this.sms = sms;
+    }
+
+    public override void Enviar(string destinatario)
+    {
+        sms.SendSMS(destinatario);
+    }
+}
+
+class EnviarPush : Notifications
+{
+
+    public void EnviarPush(string destinatario)
+    {
         globais g = globais.getInstances();
-        g.getNomeAplicacao();
-        g.getServidor();
-        g.getMaxTentativas();
     }
 }
 
-class NotificationsFactory{
+class PushExterno
+{
+    public void SendPush(string destinatario) { }
+}
 
-    public static Notifications criarNotificacao(String tipo){
+class AdaptadorPush : Notifications
+{
+    private PushExterno push;
+
+    AdaptadorPush(PushExterno push)
+    {
+        this.push = push;
+    }
+
+    public override void Enviar(string destinatario)
+    {
+        push.SendPush(destinatario);
     }
 }
 
-class ServicoCompleto{
+class NotificationsFactory
+{
 
-    public void enviarNotificacao(String tipo, String destinatario){
-        Notifications notificacao = NotificationsFactory.criarNotificacao(tipo);
-        notificacao.enviar(destinatario);
+    public static Notifications CriarNotificacao(string tipo)
+    {
+        switch (tipo)
+        {
+            case "email":
+                return new EnviarEmail();
+            case "sms":
+                return new EnviarSMS();
+            case "push":
+                return new EnviarPush();
+        }
+    }
+}
+
+class ProxyLogger : Notifications
+{
+    private Notifications baseNotificacao;
+
+    public ProxyLogger(Notifications baseNotificacao)
+    {
+        this.baseNotificacao = baseNotificacao;
+    }
+
+    public override void Enviar(string destinatario)
+    {
+        Console.WriteLine($"Proxy Logger antes de enviar a msg");
+        baseNotificacao.Enviar(destinatario);
+        Console.WriteLine($"Proxy logger depois de enviar a msg");
+    }
+}
+
+class ServicoCompleto
+{
+    public void EnviarNotificacao(string tipo, string destinatario)
+    {
+        Notifications notificacao = NotificationsFactory.CriarNotificacao(tipo);
+        ProxyLogger proxy = new ProxyLogger(notificacao);
+        proxy.Enviar(destinatario);
     }
 }
